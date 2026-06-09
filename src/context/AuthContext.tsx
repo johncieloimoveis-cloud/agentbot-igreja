09afaeb1adbcc011f22cb9e7b8de298aa57057ac
John Lobo <johncieloimoveis@gmail.com>
2026-06-09 15:35:02 -0300

    Fix: Export AuthContextType from AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from 'react';
 import { supabase } from '@/services/supabase';
 import { User } from '@supabase/supabase-js';
 
-interface AuthContextType {
+export interface AuthContextType {
   user: User | null;
   loading: boolean;
   login: (email: string, password: string) => Promise<void>;

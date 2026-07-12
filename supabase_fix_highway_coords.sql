9f1153551d90ffc65324ba9a054ba7491c075f93
John Lobo <johncieloimoveis@gmail.com>
2026-07-12 16:21:06 -0300

    fix: resetar TODOS enderecos de rodovia (sem bounding box) para reprocessamento

--- supabase_fix_highway_coords.sql
+++ supabase_fix_highway_coords.sql
@@ -1,14 +1,12 @@
--- Limpa coordenadas de pessoas cujo endereço contém rodovia/km
--- mas cujas coordenadas estão fora do Paraná (lat < -26.7 ou lat > -22.5 ou lon < -54 ou lon > -48)
--- Paraná fica entre lat -26.7 e -22.5, lon -54 e -48
+-- Reseta TODOS os endereços de rodovia/km para reprocessamento.
+-- A bounding box não é suficiente: coordenadas erradas podem cair dentro do Paraná
+-- mas em cidade errada (ex: BR 153 km 124 em Ibaiti sendo colocado em Guarapuava).
+-- Após rodar este script, execute novamente /admin/geocodificar.
 
 UPDATE people
 SET lat = NULL, lon = NULL, geocode_status = 'failed'
 WHERE (
-  address ILIKE '%br%' OR address ILIKE '%km%' OR address ILIKE '%rodovia%'
-)
-AND lat IS NOT NULL
-AND (
-  lat < -26.7 OR lat > -22.5
-  OR lon < -54.0 OR lon > -48.0
-);
+  address ILIKE '%br -%'
+  OR address ILIKE '%br-%'
+  OR address ILIKE '%br %'
+  OR addr
